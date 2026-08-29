3ab4fdeecc6e1e13c848a588c935a35ff9305e2e
Samuel Cook <email@sjc.sh>
2026-08-28 21:08:26 -0500

    revert unsupported extends in oxfmt config

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -1,4 +1,4 @@
 import { defineConfig } from "oxfmt";
 import { baseOxfmtConfig } from "sjc-ts/unstable/tooling/oxfmt";
 
-export default defineConfig({ extends: [baseOxfmtConfig] });
+export default defineConfig(baseOxfmtConfig);
